87928e723c47dabd94633dfdb961df11e8e1c96e
ROCKET19 <48097039+ROCKET19@users.noreply.github.com>
2020-10-06 15:49:17 +0530

    added image feature

--- myproject/todo/models.py
+++ myproject/todo/models.py
@@ -3,6 +3,11 @@ from django.db import models
 class Todo(models.Model):
     text = models.CharField(max_length=40)
     complete = models.BooleanField(default=False)
+<<<<<<< HEAD
+=======
+    image_url = models.CharField(max_length=2083, null=True)
+    image = models.ImageField(upload_to='pictures', blank=True)
+>>>>>>> fde8ace... added image feature
 
     def __str__(self):
         return self.text
