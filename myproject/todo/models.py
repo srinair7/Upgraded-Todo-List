a408ac17c12b6f860844d72d2f9627aff191f1e6
Srikesh Nair <srinair7@github.com>
2020-10-08 21:02:44 +0530

    checks

--- myproject/todo/models.py
+++ myproject/todo/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Todo(models.Model):
     text = models.CharField(max_length=40)
     complete = models.BooleanField(default=False)
-    image_url = models.CharField(max_length=2083, null=True)
     image = models.ImageField(upload_to='pictures', blank=True)
 
     def __str__(self):
